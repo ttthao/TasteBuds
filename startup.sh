b05fe71fa1218fcf0d51719e26c86f66c6c1baaf
Joshua H <jah148@ucsd.edu>
2017-10-21 15:56:12 -0700

    startup.py no longer automatically opens browser

--- startup.sh
+++ startup.sh
@@ -8,6 +8,4 @@ pip install -qr requirements.txt
 export GENOMELINK_CLIENT_ID=CLOmX1sWF2BRgmSgdP8L3VZlWaLqiGQnfFmVgqRl
 export GENOMELINK_CLIENT_SECRET=GGwsYiUDKB9ESvGYXL9a7c1vQEtPsBgDXWwBDPxph5cYtaD9QceB2PRBZc7b1OEOuSUXEVWcRG9CbwtsKxTM07YTkol010glqrp3xhaYcsmmBCv55SPNxububAMidVtm
 export GENOMELINK_CALLBACK_URL="http://127.0.0.1:5000/callback"
-python app.py &
-sleep 3
-python -mwebbrowser "http://127.0.0.1:5000/"
+python app.py 
